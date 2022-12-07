e15c75bb465644c92a6ce98add899a73440804fb
XDZhelheim <601251915@qq.com>
2022-12-07 11:05:14 +0800

    PL is slow

--- cross_sectional_model/pl_model.py
+++ cross_sectional_model/pl_model.py
@@ -5,6 +5,9 @@ import numpy as np
 import pandas as pd
 import datetime
 
+import warnings
+warnings.filterwarnings("ignore")
+
 def gen_xy(sequence):
     """
     Returns
@@ -122,10 +125,10 @@ class PL_MLP(pl.LightningModule):
         # self.log("Train loss", loss)
         return loss
     
-    def training_epoch_end(self, outputs) -> None:
-        avg_loss = torch.stack([x["loss"] for x in outputs]).mean()
+    # def training_epoch_end(self, outputs) -> None:
+    #     avg_loss = torch.stack([x["loss"] for x in outputs]).mean()
         
-        print(datetime.datetime.now(), "Epoch", self.current_epoch, avg_loss.cpu().numpy())
+    #     print(datetime.datetime.now(), "Epoch", self.current_epoch, avg_loss.cpu().numpy())
     
     def validation_step(self, batch, batch_idx):
         x, y=batch
@@ -135,7 +138,103 @@ class PL_MLP(pl.LightningModule):
         return loss
     
     def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=1e-4)
+        return self.optimizer
+    
+class RNNClassifier(pl.LightningModule):
+    def __init__(
+        self,
+        num_users,
+        embedding_dim=16,
+        hidden_dim=64,
+        num_layers=1,
+        dropout=0,
+        bidirectional=True,
+    ) -> None:
+        super().__init__()
+
+        self.num_users = num_users
+
+        self.user_embedding_layer = nn.Embedding(
+            num_embeddings=num_users, embedding_dim=embedding_dim, padding_idx=-1
+        )
+        
+        # self.bn = nn.BatchNorm1d(num_features=embedding_dim)
+
+        self.input_dim = embedding_dim * 2 + 1
+        self.hidden_dim = hidden_dim
+        self.bidirectional = bidirectional
+
+        self.rnn = nn.GRU(
+            input_size=self.input_dim,
+            hidden_size=hidden_dim,
+            num_layers=num_layers,
+            batch_first=True,
+            dropout=dropout,
+            bidirectional=bidirectional,
+        )
+
+        fc_input_dim = hidden_dim * 2 if bidirectional else hidden_dim
+        self.fc = nn.Sequential(
+            nn.Linear(fc_input_dim, fc_input_dim // 2),
+            nn.ReLU(inplace=True),
+            nn.Linear(fc_input_dim // 2, 2),
+        )
+
+    def forward(self, x):
+        # x: (batch_size, seq_len, 3(from, to, label))
+        # batch_size = x.shape[0]
+        # seq_len = x.shape[1]
+
+        from_user = x[:, :, 0].long()
+        to_user = x[:, :, 1].long()
+        label = x[:, :, 2][:, :, None]
+
+        from_embedding = self.user_embedding_layer(
+            from_user
+        )  # (batch_size, seq_len, embedding_dim)
+        # from_embedding = self.bn(from_embedding.permute(0, 2, 1)).permute(0, 2, 1)
+        
+        to_embedding = self.user_embedding_layer(to_user)
+        # to_embedding = self.bn(to_embedding.permute(0, 2, 1)).permute(0, 2, 1)
+        
+        input = torch.concat(
+            (from_embedding, to_embedding, label), dim=2
+        )
+
+        out, _ = self.rnn(input)
+        out = out[:, -1, :]  # (batch_size, hidden_dim)
+        out = self.fc(out)
+        out = torch.softmax(out, dim=1)
+
+        return out
+    
+    def set_criterion(self, criterion):
+        self.criterion=criterion
+        
+    def set_optimizer(self, optimizer):
+        self.optimizer=optimizer
+        
+    def training_step(self, batch, batch_idx):
+        x, y=batch
+        out=self.forward(x)
+        loss=self.criterion(out, y)
+        # self.log("Train loss", loss)
+        return loss
+    
+    # def training_epoch_end(self, outputs) -> None:
+    #     avg_loss = torch.stack([x["loss"] for x in outputs]).mean()
+        
+    #     print(datetime.datetime.now(), "Epoch", self.current_epoch, avg_loss.cpu().numpy())
+    
+    def validation_step(self, batch, batch_idx):
+        x, y=batch
+        out=self.forward(x)
+        loss=self.criterion(out, y)
+        # self.log("Val loss", loss)
+        return loss
+    
+    def configure_optimizers(self):
+        return self.optimizer
     
 if __name__ == "__main__":
     data = "../data/data_100000_distr.pkl"
@@ -148,10 +247,11 @@ if __name__ == "__main__":
         sequence, batch_size=32, train_size=0.7, val_size=0.1
     )
     
-    model=PL_MLP(num_users=5000, embedding_dim=8, hidden_dim=16)
+    # model=PL_MLP(num_users=5000, embedding_dim=8, hidden_dim=16)
+    model=RNNClassifier(num_users=5000, embedding_dim=8, hidden_dim=16)
     model.set_criterion(nn.CrossEntropyLoss())
-    model.set_optimizer(torch.optim.Adam(model.parameters(), lr=1e-4))
+    model.set_optimizer(torch.optim.Adam(model.parameters(), lr=1e-3))
 
-    trainer=pl.Trainer(accelerator="cpu", max_epochs=1000, enable_progress_bar=False, enable_checkpointing=False)
+    trainer=pl.Trainer(accelerator="cpu", max_epochs=1000, enable_progress_bar=True, enable_checkpointing=False)
     trainer.fit(model, train_loader, val_loader)
     
